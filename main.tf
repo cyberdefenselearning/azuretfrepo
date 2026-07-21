d7ca3526ed23e60d563a992cb260fab30951dce1
CyberDefenseLearning <cyberdefenselearning@GMAIL.COM>
2026-07-21 12:24:33 +0200

    Clean up duplicate variable and policy definitions

--- main.tf
+++ main.tf
@@ -1,18 +1,5 @@
-variable "target_subscription_id" {
-  type        = string
-  description = "Target Azure Subscription ID"
-}
-
-# 1. Test Policy Module (Running Now)
+# Call the policies module
 module "policies" {
   source                 = "./modules/policies"
   target_subscription_id = var.target_subscription_id
 }
-
-# 2. Future Resources (Uncomment as you build them later!)
-# module "networking" {
-#   source = "./modules/networking"
-# }
-# module "keyvault" {
-#   source = "./modules/keyvault"
-# }
--- modules/policies/main.tf
+++ modules/policies/main.tf
@@ -1,16 +1,4 @@
 variable "target_subscription_id" {
-  type = string
-}
-
-# Look up test built-in policy definition
-data "azurerm_policy_definition" "kv_rbac" {
-  display_name = "Azure Key Vault should use RBAC permission model"
-}
-
-# Assign test policy to subscription
-resource "azurerm_subscription_policy_assignment" "test_kv_rbac" {
-  name                 = "assign-test-kv-rbac"
-  display_name         = "Test Policy - Azure Key Vault RBAC"
-  subscription_id      = "/subscriptions/${var.target_subscription_id}"
-  policy_definition_id = data.azurerm_policy_definition.kv_rbac.id
+  type        = string
+  description = "Target subscription ID passed from root"
 }
